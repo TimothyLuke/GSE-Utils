51ef47df13b3bcd114396e4d248da7da40ef3040
Timothy Minahan <timothy@minahan.net>
2016-08-01 10:50:10 +0930

    c# frame to download spell library from wow external api.

---
+++ LanguageExtractor/extractor/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string json = @"{
+""id"": 192081,
+""name"": ""Ironfur"",
+""icon"": ""ability_druid_ironfur"",
+""description"": ""Increases armor by 75% for 6 sec. Multiple uses of this ability may overlap."",
+""powerCost"": ""45 Rage"",
+""castTime"": ""Instant"",
+""cooldown"": ""0.5 sec cooldown""
+}";
+            Spell s = JsonConvert.DeserializeObject<Spell>(json);
+            Console.WriteLine(s.name.Trim());
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    // Do something
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ LanguageExtractor/extractor/Spell.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extractor
+{
+
+
+
+    /// <summary>
+    ///     {
+    /// "id": 192081,
+    /// "name": "Ironfur",
+    /// "icon": "ability_druid_ironfur",
+    /// "description": "Increases armor by 75% for 6 sec. Multiple uses of this ability may overlap.",
+    /// "powerCost": "45 Rage",
+    /// "castTime": "Instant",
+    /// "cooldown": "0.5 sec cooldown"
+    /// }
+    /// </summary>
+    public class Spell
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string icon { get; set; }
+        public string description { get; set; }
+        public string powerCost { get; set; }
+        public string castTime { get; set; }
+        public string cooldown { get; set; }
+    }
+}
