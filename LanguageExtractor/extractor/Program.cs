c97ec8f1c414e5f67618474b01aae133bad725b5
Timothy Minahan <timothy@minahan.net>
2016-08-04 08:14:34 +0930

    Code Changes

--- LanguageExtractor/extractor/Program.cs
+++ LanguageExtractor/extractor/Program.cs
@@ -60,7 +60,7 @@ namespace extractor
 
                         Spell tempSpell = JsonConvert.DeserializeObject<Spell>(value);
                         bool test = string.IsNullOrWhiteSpace(tempSpell.powerCost);
-                        Console.WriteLine($"{tempSpell.id} has no Powercost {test.ToString()}");
+                       // Console.WriteLine($"{tempSpell.id} has no Powercost {test.ToString()}");
 
                         if (isPlayerSpell(tempSpell))
                         {
@@ -79,18 +79,29 @@ namespace extractor
             //process spells
             foreach (Spell p in spells)
             {
-                Console.WriteLine($"{p.id} {p.name} {p.description}");
+                //Console.WriteLine($"{p.id} {p.name} {p.description}");
 
                 foreach (ApiLocale l in locales.Info)
                 {
                     if (l.GameLocale != "enUS")
                     {
-                        string lstring = $"{l.Api}{p.id}?{apikey}&{l.Locale}";
-                        string result = client.DownloadString(lstring);
-                        Spell localspell = JsonConvert.DeserializeObject<Spell>(result);
-                        Console.WriteLine($"{localspell.id} obtained from  {l.Locale}");
-                        l.addSpell(localspell);
-                    } else
+                        try
+                        {
+                            string lstring = $"{l.Api}{p.id}?{apikey}&{l.Locale}";
+                            string result = client.DownloadString(lstring);
+                            Spell localspell = JsonConvert.DeserializeObject<Spell>(result);
+                            //Console.WriteLine($"{localspell.id} obtained from  {l.Locale}");
+                            l.addSpell(localspell);
+                        }
+#pragma warning disable CS0168 // Variable is declared but never used
+                        catch (Exception e)
+#pragma warning restore CS0168 // Variable is declared but never used
+                        {
+                            //do nothing with the spell.
+                        }
+
+                    }
+                    else
                     {
                         l.addSpellList(spells);
                     }
@@ -103,35 +114,44 @@ namespace extractor
             {
                 string forwardfilename = l.GameLocale + ".lua";
                 string reversefilename = l.GameLocale + "HASH.lua";
+                string shadowfilename = l.GameLocale + "SHADOW.lua";
 
                 StreamWriter fw = File.CreateText(forwardfilename);
                 StreamWriter rw = File.CreateText(reversefilename);
+                StreamWriter sw = File.CreateText(shadowfilename);
 
                 if (l.GameLocale == "enUS") { 
                     csv.WriteLine("\"spellID\",\"Name\",\"Description\"");
                 }
 
+
                 fw.WriteLine("local GNOME, language = ...");
                 rw.WriteLine("local GNOME, language = ...");
+                sw.WriteLine("local GNOME, language = ...");
                 fw.WriteLine("");
                 rw.WriteLine("");
+                sw.WriteLine("");
                 fw.WriteLine("language[GSTRStaticKey][\"" + l.GameLocale + "\"] = {");
                 rw.WriteLine("language[GSTRStaticHash][\"" + l.GameLocale + "\"] = {");
+                sw.WriteLine("language[GSTRStaticShadow][\"" + l.GameLocale + "\"] = {");
 
-                foreach(Spell s in l.Spells)
+                foreach (Spell s in l.Spells)
                 {
                     fw.WriteLine("	[" + s.id + "] = \"" + s.name + "\",");
                     rw.WriteLine("	[\"" + s.name + "\"] = " + s.id + ",");
-                    if(l.GameLocale == "enUS")
+                    sw.WriteLine("	[\"" + s.name.ToLower() + "\"] = " + s.id + ",");
+                    if (l.GameLocale == "enUS")
                     {
                         csv.WriteLine($"{s.id},\"{s.name}\",\"{s.description}\"");
                     }
                 }
                 fw.WriteLine("}");
                 rw.WriteLine("}");
+                sw.WriteLine("}");
 
                 fw.Close();
                 rw.Close();
+                sw.Close();
             }
             csv.Close();
 
